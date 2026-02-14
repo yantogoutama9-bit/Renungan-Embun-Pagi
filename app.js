dd028b62ca4d88d83c697b83cb77f38dd23d08a8
yantogoutama9-bit <yantogoutama9@gmail.com>
2026-02-14 14:38:29 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Ganti dengan Supabase lo
+// Ganti placeholder ini dengan Project URL & Anon Key asli dari Supabase lo
 const supabaseUrl = "https://btzffoirbqazffbgmsnh.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ0emZmb2lyYnFhemZmYmdtc25oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEwMDEwNTYsImV4cCI6MjA4NjU3NzA1Nn0.ban0dPbvGNDUJTJRwAp-EhlzXh3Uy_4dH4A43hw8Qbg";
 const supabase = supabase.createClient(supabaseUrl, supabaseKey);
@@ -30,10 +30,9 @@ async function getArticles() {
 
 getArticles();
 
-// Service Worker
+// Register service worker
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('service-worker.js')
     .then(() => console.log('Service Worker terdaftar'))
     .catch(err => console.log('Error SW:', err));
 }
-
